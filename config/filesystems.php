44c9cad0e7f356fe38723fb242ffcf5180f6aaf5
Simon Hampel <simon@hampelgroup.com>
2025-05-01 16:42:37 +1000

    we're not going to  use the Storage facade for transferring to s3, we'll use rclone, so removing that configuration

--- config/filesystems.php
+++ config/filesystems.php
@@ -14,20 +14,7 @@ return [
     */
 
     'default' => env('FILESYSTEM_DISK', 'backup'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Default Cloud Filesystem Disk
-    |--------------------------------------------------------------------------
-    |
-    | Many applications store files both locally and in the cloud. For this
-    | reason, you may specify a default "cloud" driver here. This driver
-    | will be bound as the Cloud disk implementation in the container.
-    |
-    */
-
-    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
-
+    
     /*
     |--------------------------------------------------------------------------
     | Filesystem Disks
@@ -51,18 +38,6 @@ return [
         'backup' => [
             'driver' => 'local',
             'root' => env('FILESYSTEM_LOCAL_BACKUP_PATH', storage_path('backup')),
-        ],
-
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => true,
         ],
 
     ],
