f26cc19827187d428068a896c771fd51e7a55b64
Simon Hampel <simon@hampelgroup.com>
2025-05-01 13:26:16 +1000

    reset storage path when running as a Phar to either LARAVEL_STORAGE_PATH or the current working directory if not specified

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (\Phar::running(false))
+        {
+            $this->app->useStoragePath(env('LARAVEL_STORAGE_PATH', getcwd()));
+        }
     }
 
     /**
