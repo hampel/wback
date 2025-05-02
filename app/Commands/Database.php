906cd88a4caced64b17dee3b448ba5e582cd9f5f
Simon Hampel <simon@hampelgroup.com>
2025-05-02 11:06:01 +1000

    set command alias database|db

--- app/Commands/Database.php
+++ app/Commands/Database.php
@@ -19,6 +19,15 @@ class Database extends BaseCommand
                                 {--d|dry-run : Do everything except the actual backup}
                             ';
 
+    protected function configure()
+    {
+        $this->setAliases([
+            'db',
+        ]);
+
+        parent::configure();
+    }
+
     /**
      * The console command description.
      *
