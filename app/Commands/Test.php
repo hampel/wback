7229556155eee2905c1daf4fe463ad184385fc44
Simon Hampel <simon@hampelgroup.com>
2025-05-15 07:30:10 +1000

    test command to test logging configuration

---
+++ app/Commands/Test.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Log;
+use LaravelZero\Framework\Commands\Command;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Test application configuration';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $levels = ['debug', 'info', 'notice', 'warning', 'error', 'critical', 'alert', 'emergency'];
+        foreach ($levels as $level)
+        {
+            $this->log($level, "This is a test log message");
+        }
+
+        $this->line("Log messages written - please check your logs");
+        $this->info("Logging configuration");
+        $this->info("---------------------");
+
+        $logging = collect(config('logging'))->dot();
+        foreach ($logging as $key => $value)
+        {
+            $this->line("$key: $value");
+        }
+
+        return Command::SUCCESS;
+    }
+
+    protected function log($level, $message, $logMessage = null, $context = [])
+    {
+        $verbosityMap = [
+            'debug' => OutputInterface::VERBOSITY_DEBUG,
+            'info' => OutputInterface::VERBOSITY_VERBOSE,
+            'notice' => OutputInterface::VERBOSITY_NORMAL,
+            'warning' => OutputInterface::VERBOSITY_NORMAL,
+            'error' => OutputInterface::VERBOSITY_QUIET,
+            'critical' => OutputInterface::VERBOSITY_QUIET,
+            'alert' => OutputInterface::VERBOSITY_QUIET,
+            'emergency' => OutputInterface::VERBOSITY_QUIET,
+        ];
+
+        $styleMap = [
+            'debug' => null,
+            'info' => 'info',
+            'notice' => 'comment',
+            'warning' => 'comment',
+            'error' => 'error',
+            'critical' => 'error',
+            'alert' => 'error',
+            'emergency' => 'error',
+        ];
+
+        $logMessage = $logMessage ?? $message;
+        $verbosity = $verbosityMap[$level] ?? 'warning';
+        $style = $styleMap[$level] ?? null;
+
+        Log::log($level, $logMessage, $context);
+        $this->line($message, $style, $verbosity);
+    }
+
+    /**
+     * Define the command's schedule.
+     */
+    public function schedule(Schedule $schedule): void
+    {
+         $schedule->command(static::class)->everyFiveMinutes();
+    }
+}
